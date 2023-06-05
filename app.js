c2c7e6b92c7258676cfb3e8ae7d94a91db44ff71
Benson Ndiwa <bnkimtai@gmail.com>
2023-06-05 14:53:54 +0300

    Database Connection Setup and DB creation

--- app.js
+++ app.js
@@ -2,7 +2,35 @@ let express = require('express');
 
 let mysql = require('mysql');
 
+// Create Connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodemysql'
+});
+
+// Connect
+db.connect((err)=>{
+    if(err){
+        throw err;
+    }
+    console.log("Mysql Connected")
+});
+
 let app = express();
+
+
+// Create DB
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE nodemysql';
+
+    db.query(sql, (err, result) =>{
+        if(err) throw err;
+        res.send("Database Created");
+        console.log(result)
+    })
+})
 
 
 
